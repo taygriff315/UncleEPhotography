0a8d37b290a2f1569e2ca7e0e91948613f2a397c
Taylor Griffin <titleist315@gmail.com>
2021-06-02 18:23:00 -0500

    initial commit

---
+++ UncleEPhotography/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UncleEPhotography.Startup))]
+namespace UncleEPhotography
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
